6ad9b184ccb295cba8325af33e92dc7f9453a73a
NatYarm <yarmnat7@gmail.com>
2023-03-30 17:30:52 +0300

    Add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import Footer from './components/layout/Footer';
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 
 function App() {
@@ -7,7 +10,14 @@ function App() {
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
