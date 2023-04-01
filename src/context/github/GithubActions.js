2c7f037d10482ff8e3a226ed75e5645c2ea784c3
NatYarm <yarmnat7@gmail.com>
2023-04-01 17:03:25 +0300

    Refactoring Actions & Axios

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,14 @@
+import axios from 'axios';
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
+});
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -7,48 +16,17 @@ export const searchUsers = async (text) => {
     q: text,
   });
 
-  const resp = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
+  const resp = await github.get(`/search/users?${params}`);
 
-  const { items } = await resp.json();
-
-  return items;
+  return resp.data.items;
 };
 
-// Get single user
-export const getUser = async (login) => {
-  const resp = await fetch(`${GITHUB_URL}/users/${login}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
+// Get single user and repos
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ]);
 
-  if (resp.status === 404) {
-    window.location = '/notfound';
-  }
-
-  const data = await resp.json();
-
-  return data;
-};
-
-// Get user repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: 'created',
-    per_page: 10,
-  });
-
-  const resp = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
-
-  const data = await resp.json();
-
-  return data;
+  return { user: user.data, repos: repos.data };
 };
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import RepoList from '../components/repos/RepoList';
 import Spinner from '../components/layout/Spinner';
 import GithubContext from '../context/github/GithubContext';
-import { getUser, getUserRepos } from '../context/github/GithubActions';
+import { getUserAndRepos } from '../context/github/GithubActions';
 
 const User = () => {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -14,16 +14,10 @@ const User = () => {
   useEffect(() => {
     dispatch({ type: 'SET_LOADING' });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
+      const userData = await getUserAndRepos(params.login);
       dispatch({
-        type: 'GET_USER',
+        type: 'GET_USER_AND_REPOS',
         payload: userData,
-      });
-
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({
-        type: 'GET_REPOS',
-        payload: userRepoData,
       });
     };
     getUserData();
